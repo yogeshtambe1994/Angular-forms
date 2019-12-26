6cad3d13bd8da9aa51dcdb1ed6507a936a9de75e
yogeshtambe1994 <yogeshtambe1994@gmail.com>
2019-12-26 16:15:12 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-tdf';
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
+  userModel = new User('Yogesh', 'yogesh@example.com', 9689067393, 'default', 'morning', true);
+
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+
+  }
 }
